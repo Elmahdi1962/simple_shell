8f37bbf885feab7e2748dd07d0dddc41dd96dc2a
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-14 12:36:41 +0000

    new function that checks if string is num

---
+++ utils_validator_1.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * str_is_num - checks if the string is a number
+ * @str: the string to check
+ * Return: true or false
+ */
+
+char str_is_num(char *str)
+{
+	char *string = str;
+	int firstc = 1;
+
+	while (*string != '\0')
+	{
+		if (firstc)
+		{
+			if (*string == '-' || *string == '+')
+			{
+				firstc = 0;
+				string++;
+				continue;
+			}
+		}
+		if (*string >= '0' && *string <= '9')
+			string++;
+		else
+			return (FALSE);
+	}
+	return (TRUE);
+}
