35b7b254c5042868a14951cddea808fc0c7c59e5
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-08-26 01:23:44 +0100

    Patched files

--- utils_validator_2.c
+++ utils_validator_2.c
@@ -39,7 +39,7 @@ char is_alias_name(char *str)
 			|| (*(str + i) == '\'')
 			|| (*(str + i) == '"')
 			|| (*(str + i) == '$')
-			|| ((*(str + i) == '=') && (i == 0))
+			|| ((*(str + i) == '='))
 			|| (*(str + i) == '|')
 			|| (*(str + i) == '&')
 			|| (*(str + i) == ';')
@@ -79,8 +79,7 @@ char is_alias_assignment(char *str, char **name_out, char **value_out)
 	int i = 0, j, name_len = 0, value_len = 0;
 	char is_assignment = FALSE;
 
-	for (; (str != NULL) && ((*(str + i) != '=')
-		|| ((i == 0) && (*(str + i) == '='))); i++)
+	for (; (str != NULL) && (*(str + i) != '='); i++)
 	{
 		name_len++;
 	}
